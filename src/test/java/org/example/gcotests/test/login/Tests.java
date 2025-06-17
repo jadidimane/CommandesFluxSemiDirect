b68c2c4001a0e173206057b9b9bc3bd13f98fb0f
imane.jadid <imanejadid16@gmail.com>
2025-06-17 10:24:54 +0200

    github repository

--- src/test/java/org/example/gcotests/test/login/Tests.java
+++ src/test/java/org/example/gcotests/test/login/Tests.java
@@ -1,5 +1,8 @@
 package org.example.gcotests.test.login;
 import com.opencsv.exceptions.CsvValidationException;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import org.example.gcopages.DetailCommandePage;
 import org.example.gcopages.OrderCreationPage;
 import org.example.gcopages.OrderSearchPage;
@@ -7,6 +10,7 @@ import org.example.gcotests.base.testBase;
 import org.example.utilities.Excel;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import java.io.IOException;
 import java.util.List;
@@ -14,6 +18,9 @@ import java.util.List;
 public class Tests extends testBase {
 
      @Test
+     @Story("Création d’une nouvelle commande flux semi direct")
+     @Severity(SeverityLevel.CRITICAL)
+     @DisplayName("Test de création de commande flux semi direct valide")
      public void DetailCommand() throws InterruptedException, CsvValidationException, IOException {
          menuPage=loginPage.loginIntoGCO("RHT15","RHT","911");
          OrderCreationPage order=menuPage.setCommandeFournisseur().setNewCommand();
@@ -21,6 +28,9 @@ public class Tests extends testBase {
          detail.setOrderPUQuantity();
      }
      @Test
+     @Story("Création d’une nouvelle commande flux Livraison entrepôt")
+     @Severity(SeverityLevel.CRITICAL)
+     @DisplayName("Test de création de commande flux  Livraison entrepôt valide")
      public void FillSupplierDetailLivraisonEntrepot() throws InterruptedException, CsvValidationException, IOException {
          menuPage=loginPage.loginIntoGCO("RHT15","RHT","912");
          OrderCreationPage order=menuPage.setCommandeFournisseur().setNewCommand();
